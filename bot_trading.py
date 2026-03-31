e268f1c1bc0eb6df22db2b654f694d417fc048fb
Matthieu Pettes-Duler <77773325+Matthieu-PettesDuler@users.noreply.github.com>
2026-03-31 22:04:25 +0200

    Update bot_trading.py

--- bot_trading.py
+++ bot_trading.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import pytz
 
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "8719135314:AAHkO4SsYqFcCUjzgNQ223eYBhUd0p5aySU")
-TELEGRAM_CHAT_ID  = os.environ.get("TELEGRAM_CHAT_ID", 643090969)
+TELEGRAM_CHAT_ID  = os.environ.get("TELEGRAM_CHAT_ID", 7654102743)
 ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY", "sk-ant-api03-X_7NyWc_hGPKA99enw_sg2ahBxtMASxfIFM411G85d-s-2Dftkj0yB_xW6h06jcgLKV3kgwDo0hmsNEKdZ5nOA-ihxBIwAA")
 
 SEUILS = {
